5d8fd6385e1bc06af403b2327d3a595d91312776
DSviatenko <71594821+DSviatenko@users.noreply.github.com>
2021-10-12 12:50:16 +0300

    Add files via upload

---
+++ ЛБ_5.py
@@ -0,0 +1,18 @@
+import numpy as np 
+from numpy import linalg
+
+def gauss(a, b):
+    n = len(b)
+    for k in range(0,n-1):
+        for i in range(k+1,n):
+            if a[i,k] != 0.0:
+                
+                lam = a[i,k]/a[k,k]
+                a[i,k+1:n]= a[i,k+1:n] - lam*a[k,k+1:n] 
+                b[i] = b[i] - lam*b[k]
+    
+    for k in range(n-1,-1,-1):
+        b[k] = (b[k] - np.dot(a[k,k+1:n],b[k+1:n]))/a[k,k]
+    return b 
+print(gauss(np.matrix([[1,2],[3,-2]]),np.matrix([[2],[-3]])))
+        
